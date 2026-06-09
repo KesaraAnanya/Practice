dce9a772fd9b37e9c7caf745c052dc4b328cc14d
KesaraAnanya <ananyakesara@gmail.com>
2026-06-09 09:31:38 +0530

    1

--- python_practice/list_of_nums.py
+++ python_practice/list_of_nums.py
@@ -1,10 +1,11 @@
 def unique_elements(lists):
     x = []
+    #---------------FOR LOOPS------------------
     # for i in lists:
     #     if i not in x:
     #         x.append(i)
     # return x
-
+    #---------------WHILE LOOPS------------------
     i = 0
     while i < len(lists):
         item = lists[i]
