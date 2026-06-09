ce1efe9acf943e1d4ac73806e60c105b2cbf9a0d
KesaraAnanya <ananyakesara@gmail.com>
2026-06-09 08:06:32 +0530

    1

---
+++ python_practice/reverse_vowels.py
@@ -0,0 +1,14 @@
+vowels = ['a','e','i','o','u']
+words = input("Input:")
+i = list(words)
+left , right = 0, len(words) -1
+while left < right:
+    while left < right and i[left] not in vowels:
+        left += 1
+    while left < right and i[right] not in vowels:
+        right -= 1
+    if left  < right:
+        i[left],i[right] = i[right],i[left]
+        left += 1
+        right -= 1
+print("Output:", i)
